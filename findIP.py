0252201b394dbf0d18169a11743f84a901b68942
mh7r <mhtrmhtr@protonmail.com>
2023-03-19 03:39:40 +0300

    First Commit

---
+++ findIP.py
@@ -0,0 +1,18 @@
+import json
+from urllib.request import urlopen
+from tabulate import tabulate
+from termcolor import colored
+try:
+    url = "https://ipinfo.io/json"
+    response = urlopen(url)
+    data = json.load((response))
+    table = [["IP:", data["ip"]],
+            ["Server:", data["org"]],
+            ["Sehir:", data["city"]],
+            ["Ulke:", data["country"]],
+            ["Bolge:", data["region"]]]
+    print(colored(tabulate(table), "blue"))
+except:
+    print(colored("----------------------------", "cyan"))
+    print(colored("Baglantinizi kontrol ediniz", "red"))
+    print(colored("----------------------------", "cyan"))
